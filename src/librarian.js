18215c4a74192969d05540e67c08b718995359ab
Ethan Tweitmann <ethantweitmann@gmail.com>
2021-10-16 08:50:42 -0700

    Implement the Librarian class

--- src/librarian.js
+++ src/librarian.js
@@ -1,6 +1,25 @@
 class Librarian {
-  constructor() {
-    
+  constructor(name, library) {
+    this.name = name;
+    this.library = library;
+  }
+  greetPatron(name, isMorning) {
+    if (isMorning) {
+      return `Good morning, ${name}!`;
+    } else {
+      return `Hello, ${name}!`;
+    }
+  }
+  findBook(title) {
+    if (this.library.shelves.fantasy[0].title === title) {
+      this.library.shelves.fantasy.splice(0, 1);
+      return `Yes, we have ${title}`;
+    } else {
+      return `Sorry, we do not have ${title}`;
+    }
+  }
+  calculateLateFee(daysOverdue) {
+    return Math.ceil(daysOverdue * 0.25);
   }
 }
 
